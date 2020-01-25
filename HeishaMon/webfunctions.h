a8de10782333ad3417939255286d6efd927e4870
IgorYbema <igor@ergens.org>
2020-01-25 20:24:57 +0100

    memory optimizations + json get url
    replacing actdata global variable from jsonobject to string array, replacing some strings by char * etc to optimize memory usage. Also added json output url

--- HeishaMon/webfunctions.h
+++ HeishaMon/webfunctions.h
@@ -7,8 +7,9 @@
 
 String getUptime(void);
 void setupWifi(DoubleResetDetect &drd, char* wifi_hostname, char* ota_password, char* mqtt_server, char* mqtt_port, char* mqtt_username, char* mqtt_password, bool &use_1wire);
-void handleRoot(ESP8266WebServer *httpServer, DynamicJsonDocument *actData);
-void handleTableRefresh(ESP8266WebServer *httpServer, DynamicJsonDocument *actData);
+void handleRoot(ESP8266WebServer *httpServer);
+void handleTableRefresh(ESP8266WebServer *httpServer, String actData[]);
+void handleJsonOutput(ESP8266WebServer *httpServer, String actData[]);
 void handleFactoryReset(ESP8266WebServer *httpServer);
 void handleReboot(ESP8266WebServer *httpServer);
 void handleSettings(ESP8266WebServer *httpServer, char* wifi_hostname, char* ota_password, char* mqtt_server, char* mqtt_port, char* mqtt_username, char* mqtt_password, bool &use_1wire);
