30a9b9b24c3c154513d58fadd84b9917546ce5e3
IgorYbema <igor@ergens.org>
2022-01-21 22:44:12 +0100

    fix bad header affecting bad reads

--- HeishaMon/HeishaMon.ino
+++ HeishaMon/HeishaMon.ino
@@ -288,20 +288,22 @@ bool isValidReceiveChecksum() {
 
 bool readSerial()
 {
-  if (data_length == 0 ) totalreads++; //this is the start of a new read
-
-  while ((Serial.available()) && (data_length < MAXDATASIZE)) {
-    data[data_length] = Serial.read(); //read available data and place it after the last received data
-    data_length++;
+  int len = 0;
+  while ((Serial.available()) && (len < MAXDATASIZE)) {
+    data[data_length+len] = Serial.read(); //read available data and place it after the last received data
+    len++;
     if (data[0] != 113) { //wrong header received!
       log_message((char*)"Received bad header. Ignoring this data!");
-      if (heishamonSettings.logHexdump) logHex(data, data_length);
+      if (heishamonSettings.logHexdump) logHex(data, len);
       badheaderread++;
       data_length = 0;
       return false; //return so this while loop does not loop forever if there happens to be a continous invalid data stream
     }
   }
 
+  if ((len > 0) && (data_length == 0 )) totalreads++; //this is the start of a new read
+  data_length += len;
+  
   if (data_length > 1) { //should have received length part of header now
 
     if ((data_length > (data[1] + 3)) || (data_length >= MAXDATASIZE) ) {
