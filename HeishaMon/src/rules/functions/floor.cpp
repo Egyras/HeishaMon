937040194a9bc1d6f3651a547fffdb6315612746
CurlyMoo <CurlyMoo@users.noreply.github.com>
2023-10-08 09:05:13 +0200

    Fix rules floor implementation (#120)

--- HeishaMon/src/rules/functions/floor.cpp
+++ HeishaMon/src/rules/functions/floor.cpp
@@ -33,9 +33,7 @@ int rule_function_floor_callback(struct rules_t *obj, uint16_t argc, uint16_t *a
   *ret = obj->varstack.nrbytes;
 
   unsigned int size = alignedbytes(obj->varstack.nrbytes+sizeof(struct vm_vinteger_t));
-  if((obj->varstack.buffer = (unsigned char *)REALLOC(obj->varstack.buffer, alignedbuffer(size))) == NULL) {
-    OUT_OF_MEMORY /*LCOV_EXCL_LINE*/
-  }
+
   struct vm_vinteger_t *out = (struct vm_vinteger_t *)&obj->varstack.buffer[obj->varstack.nrbytes];
   out->ret = 0;
   out->type = VINTEGER;
@@ -52,7 +50,8 @@ int rule_function_floor_callback(struct rules_t *obj, uint16_t argc, uint16_t *a
   }
 
   obj->varstack.nrbytes = size;
-  obj->varstack.bufsize = alignedbuffer(size);
+  obj->varstack.bufsize = MAX(obj->varstack.bufsize, alignedvarstack(obj->varstack.nrbytes));
+
 
   return 0;
 }
