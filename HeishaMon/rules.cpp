3451384820f0f989595d30cd8461713ded98cbfb
IgorYbema <igor@ergens.org>
2025-01-10 21:21:46 +0100

    sync up (#148)
    * remove unused dependencies
    
    * remove unused dependencies
    
    * improve note
    
    * Delete platformio.ini
    
    * Update LIBSUSED.md
    
    * Update decode.h
    
    * Update HeatPumpType.md
    
    * Update OptionalPCB.md
    
    * Update OptionalPCB.md
    
    * add bivalent heating
    
    * add bivalent heating
    
    * add bivalent heating
    
    edit heat pump to match my id,
    add  mixing valve opening
    
    * Update decode.h
    
    * fix model count
    
    * added more switch options to bivalent settings because of homeassistant-aquara plugin
    
    * Add stale action to autoclose old issues and PR's
    
    * fix: removed deprecated payload_template from yaml
    
    Update heishamon.yaml to work with version 2025.2.0
    
    * style(integrations): Fix indentation and remove trailing spaces in HASS
    
    * When initializing the memory, the parameters for the memset() function were swapped and the memory was not initialized.
    
    (the compiler warnings had pointed this out)
    
    * Update README.md
    
    * Update HeatPumpType.md
    
    * add KIT-WC09L3E5 Update HeatPumpType.md
    
    |50 | E2 D5 0C 67 00 83 92 0C 27 98 | WH-ADC0509L3E5AN | WH-WDG05LE5 | KIT-WC09L3E5 | 5 | 1ph | HP - split L-series 3kW elec heating - AN |
    
    * add KIT-WC09L3E5 Update decode.h
    
    |50 | E2 D5 0C 67 00 83 92 0C 27 98 | WH-ADC0509L3E5AN | WH-WDG05LE5 | KIT-WC09L3E5 | 5 | 1ph | HP - split L-series 3kW elec heating - AN |
    
    refer to https://github.com/Egyras/HeishaMon/pull/614
    
    * changed string representation of number to the to 52 Update decode.h
    
    "52", //string representation of number of known models (last model number + 1)
    
    * change to right number 51 Update HeatPumpType.md
    
    string representation of number 51 instead of 50
    
    * added new model combination 52 Update HeatPumpType.md
    
    |52 | E2 D5 0C 67 00 83 92 0C 27 98 | WH-SDC0509L3E5 | WH-WDG05LE5 | KIT-WC09L3E5 | 5 | 1ph | HP - split L-series 3kW elec heating |
    
    * add correct byte value for Model combination 52 Update HeatPumpType.md
    
    E2 D5 0B 34 99 83 92 0C 27 98
    
    * add model combination 52 Update decode.h
    
    WH-SDC0509L3E5
    
    E2 D5 0B 34 99 83 92 0C 27 98
    
    ---------
    
    Co-authored-by: Egyras <36902688+Egyras@users.noreply.github.com>
    Co-authored-by: mi-hol <22799428+mi-hol@users.noreply.github.com>
    Co-authored-by: HighlyCompressedAir <134067260+HighlyCompressedAir@users.noreply.github.com>
    Co-authored-by: Gosun <28876168+frankol@users.noreply.github.com>
    Co-authored-by: geduxas <gediminas.vileikis@gmail.com>
    Co-authored-by: Krilo_89 <jordyvleugel@gmail.com>
    Co-authored-by: Alexander Gil <pando855@gmail.com>
    Co-authored-by: Reichelt <reichelt@b-tu.de>
    Co-authored-by: thecem <46648579+thecem@users.noreply.github.com>

--- HeishaMon/rules.cpp
+++ HeishaMon/rules.cpp
@@ -608,8 +608,8 @@ static int8_t vm_value_set(struct rules_t *obj) {
         }
       }
 
-      memset(&cmd, 256, 0);
-      memset(&log_msg, 256, 0);
+      memset(&cmd, 0, sizeof(cmd));
+      memset(&log_msg, 0, sizeof(log_msg));
 
       if(heishamonSettings.optionalPCB) {
         //optional commands
