d8291abf4f31d73fe2855a9469c01930d5a85aad
CurlyMoo <curlymoo1@gmail.com>
2025-01-13 19:24:03 +0100

    Reverse two arguments order

--- HeishaMon/src/rules/functions/gpio.cpp
+++ HeishaMon/src/rules/functions/gpio.cpp
@@ -17,33 +17,33 @@
 
 int8_t rule_function_gpio_callback(struct rules_t *obj) {
   int8_t gpio = 0, state = 0;
-  uint8_t nr = rules_gettop(obj);
+  uint8_t nr = rules_gettop(obj), x = 1;
 
   if(nr == 1 || nr == 2) {
-    if(rules_type(obj, nr) == VINTEGER) {
-      gpio = rules_tointeger(obj, nr);
-      rules_remove(obj, nr--);
+    if(rules_type(obj, x) == VINTEGER) {
+      gpio = rules_tointeger(obj, x);
+      rules_remove(obj, x++);
 #ifdef DEBUG
       printf("\tgpio = %d\n", gpio);
 #endif
       rules_pushinteger(obj, digitalRead(gpio));
-      if(nr == 0) {
+      if(nr == 1) {
         return 0;
       }
     } else {
       return -1;
     }
   }
-  if(nr == 1) {
-    if(rules_type(obj, nr) == VINTEGER) {
-      state = rules_tointeger(obj, nr);
+  if(nr == 2) {
+    if(rules_type(obj, x) == VINTEGER) {
+      state = rules_tointeger(obj, x);
       if(state < 0 || state > 1) {
         return -1;
       }
 #ifdef DEBUG
       printf("\tstate = %d\n", state);
 #endif
-      rules_remove(obj, nr--);
+      rules_remove(obj, x++);
     } else {
       return -1;
     }
